4028d4d54a66214a062016773e136ed70b9c7ecf
Joan Rasmussen <raz.joan@gmail.com>
2022-02-03 20:49:35 -0700

    Add search by grain functionality

--- src/components/GrainResults.js
+++ src/components/GrainResults.js
@@ -11,24 +11,44 @@ const GrainResults = () => {
   
   const [grains, setGrains] = useState([])
   const [farms, setFarms] = useState([])
+  const [search, setSearch] = useState('')
+  const [filtered, setFiltered] = useState([])
   
   const grainCards = grains.map(grain => {
     let farm = farms.find(farm => grain.farm_id === farm.id)
     return (
       <Grain key={grain.id} grain={grain} farm={farm}/>
       )
-    })
+  })
+
+  const filteredCards = filtered.map(grain => {
+    let farm = farms.find(farm => grain.farm_id === farm.id)
+    return (
+      <Grain key={grain.id} grain={grain} farm={farm} />
+    )
+  })
     
   useEffect(() => {
     setFarms(data.farms)
     setGrains(data.grains)
   })
 
+  const handleChange = (searchText) => {
+    const grainsFiltered = grains.filter(grain => grain.name.toLowerCase().includes(searchText))
+    // console.log(grainsFiltered)
+    setFiltered(grainsFiltered)
+    setSearch(searchText)
+    // console.log(grains)
+  }
+
   return (
     <div className="grain-browse-view">
-      <Search />
+      <Search handleChange={handleChange}/>
       <section className="grains-container">
-          { (grains && farms) ? grainCards : <p className='loading-message'>Loading . . .</p>}
+        {(search && !filteredCards.length) && <p>No grains match the current search. Please start over!</p>}
+        {search ? filteredCards :
+          (grains && farms) ? grainCards : <p className='loading-message'>Loading . . .</p>
+        }
       </section>  
     </div>
   )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,17 @@
 import React, {useState} from 'react';
 import '../css/Search.css';
 
-const Search = () => {
+const Search = ({handleChange}) => {
 
   const [text, setText] = useState("")
   // can reuse this component according to params?
   // i.e. if path is "/browse", assign labelName a certain value
   // const labelName = "Filter By Grain Type" || "Filter by Farm Name"
+  
+  const updateChange = (event) => {
+    setText(event.target.value)
+    handleChange(event.target.value.toLowerCase())
+  }
 
   return (
     <form className="search-bar">
@@ -17,7 +22,7 @@ const Search = () => {
         id="text"
         name="text"
         value={text}
-        onChange={(event) => {setText(event.target.value)}}
+        onChange={(event) => {updateChange(event)}}
       />
       <button>SEARCH</button>
     </form>
