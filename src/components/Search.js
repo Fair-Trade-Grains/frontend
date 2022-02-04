8e414c0bbba36c24cd5280e2a9e362360bacf6b8
Joan Rasmussen <raz.joan@gmail.com>
2022-02-03 20:14:56 -0700

    Build search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/Search.css';
 
+const Search = () => {
+
+  const [text, setText] = useState("")
+  // can reuse this component according to params?
+  // i.e. if path is "/browse", assign labelName a certain value
+  // const labelName = "Filter By Grain Type" || "Filter by Farm Name"
+
+  return (
+    <form className="search-bar">
+      {/* <label htmlFor="text">{labelName}: </label> */}
+      <label htmlFor="text">Search by Grain Type: </label>
+      <input
+        type="text"
+        id="text"
+        name="text"
+        value={text}
+        onChange={(event) => {setText(event.target.value)}}
+      />
+      <button>SEARCH</button>
+    </form>
+  )
+}
 
 export default Search;
