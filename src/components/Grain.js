2729807fc949dad06f16bd68f6ee4382b7a7a1fd
Joan Rasmussen <raz.joan@gmail.com>
2022-02-03 20:15:39 -0700

    Build Grain component

--- src/components/Grain.js
+++ src/components/Grain.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import '../css/Grain.css';
 
+const Grain = ({grain, farm}) => {
+  return (
+    <div className="grain-result">
+      <h2>{grain.name}</h2>
+      <div>
+        <p>Protein: {grain.protein}</p>
+        <p>Moisture: {grain.moisture}</p>
+        <p>Test Weight: {grain.test_weight}</p>
+        <p>Falling Number: {grain.falling_number}</p>
+      </div>
+      <div>
+        <h3>Farmer Name: {farm.name}</h3>
+        <p>Location: {farm.region}</p>
+      </div>
+      <button>View Details!</button>
+    </div>
+  )
+}
 
 export default Grain;
