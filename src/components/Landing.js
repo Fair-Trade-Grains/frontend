5b534e6babd45582125ed7b6bdde76fbda14dd12
Joan Rasmussen <raz.joan@gmail.com>
2022-02-06 20:09:38 -0700

    Style Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,15 +4,17 @@ import '../css/Landing.css';
 
 const Landing = () => {
   return (
-    <div>
+    <div className="landing-container">
       <div className='grains-portal'>
-        <p>Check out some 'saawheat' grains!</p>
-        <Link to='grains'>Connect</Link>
+        <p className="landing-portal-title">Check out some 'saawheat' grains!</p>
+        <Link to='grains' className="landing-link">Connect</Link>
       </div>
       <div className='farmers-portal'>
-        <p>Or perhaps you have some grains to peddle?</p>
-        <Link to='create-farmer'>New Farm</Link>
-        <Link to='farms'>Existing Farm</Link>
+        <p className="landing-portal-title">Or perhaps you have some grains to peddle?</p>
+        <div className="landing-btn-container">
+          <Link to='create-farmer' className="landing-link">New Farm</Link>
+          <Link to='farms' className="landing-link">Existing Farm</Link>
+        </div>
       </div>
     </div>
   )
