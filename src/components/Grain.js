0affdb1fa1486605f662960f612a9312a86858ef
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2022-02-07 15:19:36 -0500

    Add icons and style grain results

--- src/components/Grain.js
+++ src/components/Grain.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../css/Grain.css';
 import { useNavigate } from 'react-router'
+import wheatIcon from '../assets/wheat.svg'
+import statsIcon from '../assets/stats.svg'
+import barnIcon from '../assets/barn.svg'
 
 
 const Grain = ({ grain, farm }) => {
-  
+
   const navigate = useNavigate()
 
   const navigateToProfile = (event) => {
@@ -14,18 +17,27 @@ const Grain = ({ grain, farm }) => {
 
   return (
     <div className="grain-result">
-      <h2>{grain.name}</h2>
-      <div>
-        <p>Protein: {grain.protein}</p>
-        <p>Moisture: {grain.moisture}</p>
-        <p>Test Weight: {grain.test_weight}</p>
-        <p>Falling Number: {grain.falling_number}</p>
+      <div className='grain-name'>
+        <img className='wheat-icon' src={wheatIcon} />
+        <h2>{grain.name}</h2>
+      </div>
+      <div className='grain-stats'>
+        <img className='stats-icon' src={statsIcon} />
+        <div>
+          <p>Protein: {grain.protein}</p>
+          <p>Moisture: {grain.moisture}</p>
+          <p>Test Weight: {grain.test_weight}</p>
+          <p>Falling Number: {grain.falling_number}</p>
+        </div>
       </div>
       {farm &&
         <>
-          <div>
-            <h3>Farmer Name: {farm.name}</h3>
-            <p>Location: {farm.region}</p>
+          <div className='farmer-stats'>
+            <img className='barn-icon' src={barnIcon} />
+            <div>
+              <h3>{farm.name}</h3>
+              <p>{farm.region}</p>
+            </div>
           </div>
         <button id={farm.id} onClick={(event) => {navigateToProfile(event)}}>View Details!</button>
         </>
