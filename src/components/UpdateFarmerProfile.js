6d146ce96a661778bb684c2c6d37e39353da43fe
Joan Rasmussen <raz.joan@gmail.com>
2022-02-07 16:18:08 -0700

    Build and basic style for UpdateFarmerProfile

--- src/components/UpdateFarmerProfile.js
+++ src/components/UpdateFarmerProfile.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import ReactModal from 'react-modal'
-import {useParams} from 'react-router'
+import { Link } from 'react-router-dom'
 import '../css/UpdateFarmerProfile.css'
 import NewGrainForm from './NewGrainForm'
+import FarmerProfile from './FarmerProfile'
 
 ReactModal.setAppElement('#root')
 
 const UpdateFarmerProfile = () => {
 
-  const { farmID } = useParams()
   const [showModal, setShowModal] = useState(false)
 
   const handleOpendModal = () => {
@@ -21,9 +21,16 @@ const UpdateFarmerProfile = () => {
 
   return (
     <div className="update-farmer-view">
-      <button onClick={() => { handleOpendModal() }}>Open Modal</button>
+      <div className="nav-btn-container">
+        <Link to="/farms" className="update-farmer-nav-btn">Return to Farm List</Link>
+        <button onClick={() => { handleOpendModal() }} className="update-farmer-nav-btn">Add a New Grain</button>
+      </div>
+      <section className="farmer-details-container">
+        <FarmerProfile />
+      </section>
       <ReactModal isOpen={showModal}>
-        <button onClick={() => { handleCloseModal() }}>Close Modal</button>
+        <button onClick={() => { handleCloseModal() }} className="update-farmer-nav-btn">Close</button>
+        <NewGrainForm />
       </ReactModal>
     </div>
   )
