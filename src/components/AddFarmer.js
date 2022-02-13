b831acaecfc34c427ab2867f2bbf6a308f82a55c
Joan Rasmussen <raz.joan@gmail.com>
2022-02-13 14:08:57 -0700

    Refactor and fix error handling on create-farmer

--- src/components/AddFarmer.js
+++ src/components/AddFarmer.js
@@ -1,29 +1,42 @@
-import React from 'react';
-import '../css/NewProfileForm.css'
+import React, { useState } from 'react';
+import '../css/NewProfileForm.css';
 import { gql, useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router';
 
 const CREATE_FARMER = gql`
 mutation CreateFarmer($input: CreateFarmerInput!){
   createFarmer (input: $input) {
-          name
-          bio
-          }
-      }
+      name
+      bio
+    }
+  }
 `;
 
-const AddFarmer = ({ profile }) => {
-  let input;
+const AddFarmer = ({ profile, clearInputs }) => {
+
+  const navigate = useNavigate()
   const [createFarmer, { data, loading, error }] = useMutation(CREATE_FARMER);
+  const [invalidField, setInvalidField] = useState(false);
 
   if (loading) return 'Submitting...';
   if (error) return `Submission error! ${error.message}`;
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (profile.name && profile.email && profile.region && profile.bio) {
+      createFarmer({ variables: { input: profile } });
+      setInvalidField(false);
+      clearInputs();
+      navigate('/farms');
+    } else {
+      setInvalidField(true);
+    }
+  }
+  
   return (
     <div>
-        <button className='profile-submit-btn' onClick={e => {
-          e.preventDefault();
-          createFarmer({ variables: { input: profile }});
-        }} type="submit">Submit</button>
+      <button className='profile-submit-btn' onClick={e => { handleSubmit(e) }} type='submit'>Submit</button>
+      {invalidField && <p>Please fill out all required fields.</p>}
     </div>
   );
 }
--- src/components/NewProfileForm.js
+++ src/components/NewProfileForm.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import '../css/NewProfileForm.css';
 import ReactModal from 'react-modal';
-import usdaRegionMap from '../assets/usdaRegionMap.png'
-import { gql, useMutation } from '@apollo/client';
+import usdaRegionMap from '../assets/usdaRegionMap.png';
+import AddFarmer from './AddFarmer';
 
-import AddFarmer from './AddFarmer'
-
-ReactModal.setAppElement('#root')
+ReactModal.setAppElement('#root');
 
 class NewProfileForm extends Component {
   constructor() {
@@ -19,20 +17,21 @@ class NewProfileForm extends Component {
       region: '',
       bio: '',
       photo_path: '',
-      showModal: false
+      showModal: false,
+      profile: {}
     }
   }
 
-  handleOpendModal = () => {
-    this.setState({ showModal: true })
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
   }
 
   handleCloseModal = () => {
-    this.setState({ showModal: false })
+    this.setState({ showModal: false });
   }
 
   handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
+    this.setState({ [event.target.name]: event.target.value });
     this.updateCurrentProfile();
   }
 
@@ -44,8 +43,10 @@ class NewProfileForm extends Component {
       address: '',
       region: '',
       bio: '',
-      photo_path: ''
-    })
+      photo_path: '',
+      showModal: false,
+      profile: {}
+    });
   }
 
   updateCurrentProfile = () => {
@@ -58,113 +59,103 @@ class NewProfileForm extends Component {
       bio: this.state.bio,
       photoUrl: this.state.photo_path
     }
-    this.setState({ profile: newProfile })
+    this.setState({ profile: newProfile });
   }
 
-  submitProfile = event => {
-    event.preventDefault();
-    if (this.state.name && this.state.email && this.state.region && this.state.bio) {
+  render() {
+    return (
+      <div className='new-profile-form'>
+        <button onClick={() => { this.handleOpenModal() }}>See map of the regions!</button>
+        <form className='new-farm-form'>
+          <label htmlFor='name'>Your name or your farm's name: *required*</label>
+          <input className='new-farm-form-input'
+            type='text'
+            id='name'
+            placeholder='Smith Family Farm'
+            name='name'
+            value={this.state.name}
+            onChange={event => this.handleChange(event)}
+            required
+          />
+          <label htmlFor='email'>Your business email: *required*</label>
+          <input className='new-farm-form-input'
+            type='text'
+            id='email'
+            placeholder='example@email.com'
+            name='email'
+            value={this.state.email}
+            onChange={event => this.handleChange(event)}
+            required
+          />
+          <label htmlFor='business_phone'>Your business phone number: </label>
+          <input className='new-farm-form-input'
+            type='text'
+            id='business_phone'
+            placeholder='(xxx) xxx - xxxx'
+            name='business_phone'
+            value={this.state.business_phone}
+            onChange={event => this.handleChange(event)}
+          />
+          <label htmlFor='address'>Your business address: </label>
+          <input className='new-farm-form-input'
+            type='text'
+            id='address'
+            placeholder='123 County Road, Denver, CO, 12345'
+            name='address'
+            value={this.state.address}
+            onChange={event => this.handleChange(event)}
+          />
+          <label htmlFor='region'>Select your region (see the reference map above this form): *required*</label>
+          <select className='new-farm-form-input'
+            id='region'
+            name='region'
+            value={this.state.region}
+            onChange={event => this.handleChange(event)}
+            required
+          >
+            <option value=''>~~ Please choose an option ~~</option>
+            <option value='Northeastern'>Northeastern</option>
+            <option value='Eastern Mountain'>Eastern Mountain</option>
+            <option value='Southern'>Southern</option>
+            <option value='Great Lakes'>Great Lakes</option>
+            <option value='Upper Midwest'>Upper Midwest</option>
+            <option value='Heartland'>Heartland</option>
+            <option value='Delta'>Delta</option>
+            <option value='Northern Plains'>Northern Plains</option>
+            <option value='Southern Plains'>Southern Plains</option>
+            <option value='Mountain'>Mountain</option>
+            <option value='Northwest'>Northwest</option>
+            <option value='Pacific'>Pacific</option>
+          </select>
+          <label htmlFor='bio'>Personal and/or farm bio: *required*</label>
+          <textarea className='new-farm-form-input'
+            id='bio'
+            placeholder='Tell us a little about your organization and your farming practices! (1000 characters or less)'
+            name='bio'
+            rows='6'
+            value={this.state.bio}
+            onChange={event => this.handleChange(event)}
+            required
+          ></textarea>
+          <label htmlFor='photo_path'>Add a photo: </label>
+          <input className='new-farm-form-input'
+            type='file'
+            id='photo_path'
+            name='photo_path'
+            accept='image/png, image/jpeg'
+            value={this.state.photo_path}
+            onChange={event => this.handleChange(event)}
+          />
+          <AddFarmer profile={this.state.profile} clearInputs={this.clearInputs}/>
+        </form>
 
-      // here's where we're gonna write what happens when you submit a profile!
-      // this.clearInputs();
-    }
-  }
-
-render(){
-  return(
-    <div className='new-profile-form'>
-      <button onClick={() => { this.handleOpendModal() }}>See map of the regions!</button>
-      <form className="new-farm-form">
-        <label htmlFor='name'>Your name or your farm's name: *required*</label>
-        <input className="new-farm-form-input"
-          type='text'
-          id='name'
-          placeholder='Smith Family Farm'
-          name='name'
-          value={this.state.name}
-          onChange={event => this.handleChange(event)}
-          required
-        />
-        <label htmlFor='email'>Your business email: *required*</label>
-        <input className="new-farm-form-input"
-          type='text'
-          id='email'
-          placeholder='example@email.com'
-          name='email'
-          value={this.state.email}
-          onChange={event => this.handleChange(event)}
-          required
-        />
-        <label htmlFor='business_phone'>Your business phone number: </label>
-        <input className="new-farm-form-input"
-          type='text'
-          id='business_phone'
-          placeholder='(xxx) xxx - xxxx'
-          name='business_phone'
-          value={this.state.business_phone}
-          onChange={event => this.handleChange(event)}
-        />
-        <label htmlFor='address'>Your business address: </label>
-        <input className="new-farm-form-input"
-          type='text'
-          id='address'
-          placeholder='123 County Road, Denver, CO, 12345'
-          name='address'
-          value={this.state.address}
-          onChange={event => this.handleChange(event)}
-        />
-        <label htmlFor='region'>Select your region (see the reference map above this form): *required*</label>
-        <select className="new-farm-form-input"
-          id='region'
-          name='region'
-          value={this.state.region}
-          onChange={event => this.handleChange(event)}
-          required
-        >
-          <option value=''>~~ Please choose an option ~~</option>
-          <option value='Northeastern'>Northeastern</option>
-          <option value='Eastern Mountain'>Eastern Mountain</option>
-          <option value='Southern'>Southern</option>
-          <option value='Great Lakes'>Great Lakes</option>
-          <option value='Upper Midwest'>Upper Midwest</option>
-          <option value='Heartland'>Heartland</option>
-          <option value='Delta'>Delta</option>
-          <option value='Northern Plains'>Northern Plains</option>
-          <option value='Southern Plains'>Southern Plains</option>
-          <option value='Mountain'>Mountain</option>
-          <option value='Northwest'>Northwest</option>
-          <option value='Pacific'>Pacific</option>
-        </select>
-        <label htmlFor='bio'>Personal and/or farm bio: *required*</label>
-        <textarea className="new-farm-form-input"
-          id='bio'
-          placeholder='Tell us a little about your organization and your farming practices! (1000 characters or less)'
-          name='bio'
-          rows='6'
-          value={this.state.bio}
-          onChange={event => this.handleChange(event)}
-          required
-        ></textarea>
-        <label htmlFor='photo_path'>Add a photo: </label>
-        <input className="new-farm-form-input"
-          type='file'
-          id='photo_path'
-          name='photo_path'
-          accept='image/png, image/jpeg'
-          value={this.state.photo_path}
-          onChange={event => this.handleChange(event)}
-        />
-        <AddFarmer profile={this.state.profile} />
-      </form>
-
-      <ReactModal isOpen={this.state.showModal} className="map-modal-container">
-        <button onClick={() => { this.handleCloseModal() }} className="update-farmer-nav-btn">Close</button>
-        <img className="usda-region-map" src={usdaRegionMap} alt="USDA Agriculture Regions Map"/>
-      </ReactModal>
+        <ReactModal isOpen={this.state.showModal} className='map-modal-container'>
+          <button onClick={() => { this.handleCloseModal() }} className='update-farmer-nav-btn'>Close</button>
+          <img className='usda-region-map' src={usdaRegionMap} alt='USDA Agriculture Regions Map' />
+        </ReactModal>
       </div>
-    )
+    );
   }
-
 }
 
 export default NewProfileForm;
