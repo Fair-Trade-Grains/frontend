47bf7f5714770e68473740a4fcbd6da6c01699f6
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2022-02-13 12:10:49 -0500

    Style landing page and grain results

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,23 +5,26 @@ const Search = ({handleChange}) => {
 
   const [text, setText] = useState("")
   const labelText = window.location.href.includes('grains') ? 'Grain Type' : 'Farm Name'
-  
+
   const updateChange = (event) => {
     setText(event.target.value)
     handleChange(event.target.value.toLowerCase())
   }
 
   return (
-    <form className="search-bar">
-      <label htmlFor="text">{`Search by ${labelText}:`} </label>
-      <input
-        type="text"
-        id="text"
-        name="text"
-        value={text}
-        onChange={(event) => {updateChange(event)}}
-      />
-    </form>
+    <div className='search-container'>
+      <form className="search-bar">
+        <label htmlFor="text">{`Search by ${labelText}:`} </label>
+        <input
+          className='search-field'
+          type="text"
+          id="text"
+          name="text"
+          value={text}
+          onChange={(event) => {updateChange(event)}}
+        />
+      </form>
+    </div>
   )
 }
 
