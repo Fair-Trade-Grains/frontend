6a9db615a9c848741dcb8471da916088e3d5bf72
Joan Rasmussen <raz.joan@gmail.com>
2022-02-13 14:09:12 -0700

    Refactor and fix error handling on new-grain

--- src/components/NewGrainForm.js
+++ src/components/NewGrainForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../css/NewGrainForm.css';
-
 import AddGrain from './AddGrain';
 
 class NewGrainForm extends Component {
@@ -12,17 +11,18 @@ class NewGrainForm extends Component {
       test_weight: '',
       moisture: '',
       falling_number: '',
-      farmers_notes: ''
+      farmers_notes: '',
+      grainProfile: {}
     }
   }
 
   handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
+    this.setState({ [event.target.name]: event.target.value });
     this.updateGrainProfile();
   }
 
   updateGrainProfile = () => {
-    const farmID = this.props.farmId
+    const farmID = this.props.farmId;
     const newGrain = {
       farmerId: parseInt(farmID),
       name: this.state.name,
@@ -32,7 +32,7 @@ class NewGrainForm extends Component {
       fallingNumber: parseFloat(this.state.falling_number),
       farmersNotes: this.state.farmers_notes
     }
-    this.setState({ grainProfile: newGrain })
+    this.setState({ grainProfile: newGrain });
   }
 
   clearInputs = () => {
@@ -42,107 +42,97 @@ class NewGrainForm extends Component {
       test_weight: '',
       moisture: '',
       falling_number: '',
-      farmers_notes: ''
+      farmers_notes: '',
+      grainProfile: {}
     })
   }
 
-  submitProfile = event => {
-    event.preventDefault();
-    if (this.state.name && this.state.protein && this.state.test_weight && this.state.moisture && this.state.falling_number) {
-      // the above farmID is being got from the UpdateFarmerProfile useParams and passed in to this form as props via farmId
-      // console.log('farmID inside submit method: ', farmID)
-      // here's where we're gonna write what happens when you submit a profile!
-      this.clearInputs();
-    }
-  }
-
-render(){
-  return(
-    <div className='new-grain-container'>
-      <form className='new-grain-form'>
-        <label htmlFor='name'>Grain name/type: *required*</label>
-        <input
-          className='new-grain-input'
-          type='text'
-          placeholder='Turkey Red Wheat'
-          id='name'
-          name='name'
-          value={this.state.name}
-          onChange={event => this.handleChange(event)}
-          required
-        />
-        <label htmlFor='proteiin'>Protein: *required*</label>
-        <input
-          className='new-grain-input'
-          type='number'
-          placeholder='12.0'
-          id='protein'
-          name='protein'
-          min='8'
-          max='18'
-          step='0.1'
-          value={this.state.protein}
-          onChange={event => this.handleChange(event)}
-          required
-        />
-        <label htmlFor='test_weight'>Test Weight: *required*</label>
-        <input
-          className='new-grain-input'
-          type='number'
-          placeholder='60'
-          id='test_weight'
-          name='test_weight'
-          min='50'
-          max='64'
-          step='1'
-          value={this.state.test_weight}
-          onChange={event => this.handleChange(event)}
-          required
-        />
-        <label htmlFor='moisture'>Moisture: *required*</label>
-        <input
-          className='new-grain-input'
-          type='number'
-          placeholder='11.5'
-          name='moisture'
-          id='moisture'
-          min='8'
-          max='15'
-          step='0.1'
-          value={this.state.moisture}
-          onChange={event => this.handleChange(event)}
-          required
-        />
-        <label htmlFor='falling_number'>Falling Number: *required*</label>
-        <input
-          className='new-grain-input'
-          type='number'
-          placeholder='300'
-          id='falling_number'
-          name='falling_number'
-          min='200'
-          max='400'
-          step='10'
-          value={this.state.falling_number}
-          onChange={event => this.handleChange(event)}
-          required
-        />
-        <label htmlFor='farmers_notes'>Any notes to share on this grain: </label>
-        <input
-          className='new-grain-input'
-          type='text'
-          placeholder='Description or other notes'
-          id='farmers_notes'
-          name='farmers_notes'
-          value={this.state.farmers_notes}
-          onChange={event => this.handleChange(event)}
+  render() {
+    return (
+      <div className='new-grain-container'>
+        <form className='new-grain-form'>
+          <label htmlFor='name'>Grain name/type: *required*</label>
+          <input
+            className='new-grain-input'
+            type='text'
+            placeholder='Turkey Red Wheat'
+            id='name'
+            name='name'
+            value={this.state.name}
+            onChange={event => this.handleChange(event)}
+            required
           />
-        <AddGrain grainProfile={this.state.grainProfile} />
+          <label htmlFor='proteiin'>Protein: *required*</label>
+          <input
+            className='new-grain-input'
+            type='number'
+            placeholder='12.0'
+            id='protein'
+            name='protein'
+            min='8'
+            max='18'
+            step='0.1'
+            value={this.state.protein}
+            onChange={event => this.handleChange(event)}
+            required
+          />
+          <label htmlFor='test_weight'>Test Weight: *required*</label>
+          <input
+            className='new-grain-input'
+            type='number'
+            placeholder='60'
+            id='test_weight'
+            name='test_weight'
+            min='50'
+            max='64'
+            step='1'
+            value={this.state.test_weight}
+            onChange={event => this.handleChange(event)}
+            required
+          />
+          <label htmlFor='moisture'>Moisture: *required*</label>
+          <input
+            className='new-grain-input'
+            type='number'
+            placeholder='11.5'
+            name='moisture'
+            id='moisture'
+            min='8'
+            max='15'
+            step='0.1'
+            value={this.state.moisture}
+            onChange={event => this.handleChange(event)}
+            required
+          />
+          <label htmlFor='falling_number'>Falling Number: *required*</label>
+          <input
+            className='new-grain-input'
+            type='number'
+            placeholder='300'
+            id='falling_number'
+            name='falling_number'
+            min='200'
+            max='400'
+            step='10'
+            value={this.state.falling_number}
+            onChange={event => this.handleChange(event)}
+            required
+          />
+          <label htmlFor='farmers_notes'>Any notes to share on this grain: </label>
+          <input
+            className='new-grain-input'
+            type='text'
+            placeholder='Description or other notes'
+            id='farmers_notes'
+            name='farmers_notes'
+            value={this.state.farmers_notes}
+            onChange={event => this.handleChange(event)}
+          />
+          <AddGrain grainProfile={this.state.grainProfile} clearInputs={this.clearInputs} closeModal={ this.props.closeModal}/>
         </form>
       </div>
-    )
+    );
   }
-
 }
 
 export default NewGrainForm;
